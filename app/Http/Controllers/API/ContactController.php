d2da272a39b08d134ce851e600ebff4da3b6cfe4
Arief <mgi.official1@gmail.com>
2022-06-15 01:23:18 +0700

    initial commit

---
+++ app/Http/Controllers/API/ContactController.php
@@ -0,0 +1,116 @@
+<?php
+
+namespace App\Http\Controllers\API;
+
+use App\Models\Contact;
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+use App\Repository\ContactRepository;
+
+class ContactController extends Controller
+{
+    private $contactRepository;
+
+    public function __construct(ContactRepository $contactRepositry)
+    {
+        $this->contactRepository = $contactRepositry;
+    }
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $contacts = $this->contactRepository->getAll();
+        return response()->json([
+            'success' => true,
+            'data' => $contacts
+        ], 200);
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+        $contactDetails = $request->only([
+            'name', 'number'
+        ]);
+        return response()->json([
+            'success' => true,
+            'data' => $this->contactRepository->createContact($contactDetails)
+        ], 201);
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        //get by id
+        $contact = $this->contactRepository->getById($id);
+        return response()->json([
+            'success' => true,
+            'data' => $contact
+        ], 200);
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Contact $request, $contactId)
+    {
+        $contactId = $request->route('id');
+        $contactDetails = $request->only([
+            'name', 'number', 'is_active'
+        ]);
+        return response()->json([
+            'success' => true,
+            'data' => $this->contactRepository->updateContact($contactId, $contactDetails)
+        ], 200);
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        //
+    }
+}
---
+++ app/Repository/ContactRepository.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace App\Repository;
+
+use App\Models\Contact;
+
+class ContactRepository
+{
+    public function getAll()
+    {
+        $contacts = Contact::orderBy('id')->where('is_active', 1)->get()->map(function ($contact) {
+            return $this->format($contact);
+        });
+        return $contacts;
+    }
+
+    public function getById($id)
+    {
+        $contact = Contact::query()->where('id', $id)->firstOrFail();
+        return $this->format($contact);
+    }
+
+    public function createContact(array $contactDetails)
+    {
+        $contact = Contact::create($contactDetails);
+        return $this->format($contact);
+    }
+
+    public function updateContact($id, array $contactDetails)
+    {
+        $contact = Contact::query()->whereId($id)->update($contactDetails);
+        return $this->format($contact);
+    }
+
+    public function format($contact)
+    {
+        return [
+            'contact_id' => $contact->id,
+            'name' => $contact->name,
+            'phone_number' => $contact->number,
+            'is_active' => $contact->is_active ? 'active' : 'inactive'
+        ];
+    }
+}
